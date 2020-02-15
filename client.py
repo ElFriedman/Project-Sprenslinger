d5cd03e5a5c80688fa7df6bf0f54bf29eac69763
mnwxy <william.k.tao@gmail.com>
2020-02-15 10:39:16 -0800

    Update client.py

--- client.py
+++ client.py
@@ -1,13 +1,13 @@
-import os
-from socket import *
-host = "10.40.30.45" # set to IP address of target computer
-port = 13000
-addr = (host, port)
-UDPSock = socket(AF_INET, SOCK_DGRAM)
+import sys
+from socket import socket, AF_INET, SOCK_DGRAM
+
+SERVER_IP   = '10.40.30.45'
+PORT_NUMBER = 5000
+SIZE = 1024
+print ("Test client sending packets to IP {0}, via port {1}\n".format(SERVER_IP, PORT_NUMBER))
+
+mySocket = socket( AF_INET, SOCK_DGRAM )
+mySocket.connect((SERVER_IP,PORT_NUMBER))
 while True:
-    data = raw_input("Enter message to send or type 'exit': ")
-    UDPSock.sendto(data, addr)
-    if data == "exit":
-        break
-UDPSock.close()
-os._exit(0)
+        mySocket.send('cool')
+sys.exit()
